8e28159c748b07dfac4f4449b7385dd5f7a9b87b
klontaler <fcknguglyandsotired@gmail.com>
2022-09-08 20:48:37 +1000

    bug fixed

--- script.js
+++ script.js
@@ -9,6 +9,9 @@ let sectionSI = document.getElementById('salaryInput');
 let sectionClar = document.getElementById('clarification');
 let sectionR = document.getElementById('result');
 
+let FBCSI = document.getElementById('FBCSI');
+let FBCClar = document.getElementById('FBCClar');
+
 // html functions
 function checkNumber(key) {
     return (key >= '0' && key <= '9') || key == 'ArrowLeft' || key == 'ArrowRight' || key == 'Delete' || key == 'Backspace';
@@ -20,15 +23,21 @@ function checkNumber(key) {
 // main code
 buttonSI.onclick = function() {
     sectionSI.style.display = 'none'; 
+    FBCSI.style.display = 'none'
+    FBCClar.style.display = 'flex'
+    sectionClar.style.display = 'flex';
 }
 
 buttonClar.onclick = function() {
     sectionClar.style.display = 'none'; 
-    sectionR.style.display = 'none'; 
+    sectionR.style.display = 'grid'; 
+    buttonBack.style.display = 'block';
+    FBCClar.style.display = 'none'
 }
 
 buttonBack.onclick = function() {
     sectionSI.style.display = 'flex';
     sectionR.style.display = 'none';
+    FBCSI.style.display = 'f;ex'
     inputSal.innerHTML = '';
 }
